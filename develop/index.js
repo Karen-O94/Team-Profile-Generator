6ecf5a4f77b14217009491af377d2f6b4ad25885
Karen Opoku <karenyaa@live.co.uk>
2021-02-23 21:55:47 +0000

    added individual js files for parent and subclasses

--- develop/index.js
+++ develop/index.js
@@ -1,3 +1,13 @@
 // List of packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+
+inquirer.prompt ([
+    {
+        type:'input',
+        name:'title',
+        message:'',
+    }
+
+])
